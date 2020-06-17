3c5358ddd887b3ecd41e91deb5b3d22b94e951bd
leandrosouzaa <leandror.dev@gmail.com>
2020-06-17 14:27:37 -0300

    home screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import {Container, Teste} from './styles';
+import {Image} from 'react-native';
+import {Pokemon} from '../../components';
+import {Container, Title, PokeList} from './styles';
 
-const Home: React.FC = () => (
-   <Container>
-      <Teste>Pokedex</Teste>
-   </Container>
-);
+const Home: React.FC = () => {
+   return (
+      <Container
+         imageStyle={{
+            width: 163,
+            height: 192,
+            position: 'absolute',
+            left: undefined,
+         }}
+         source={require('../../assets/images/pokeball.jpg')}>
+         <Title>Pokedex</Title>
+         <PokeList>
+            <Pokemon />
+            <Pokemon />
+         </PokeList>
+      </Container>
+   );
+};
 
 export default Home;
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components/native';
+import {StatusBar} from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.ImageBackground`
    flex: 1;
    background: #ffffff;
+   padding: 0 25px;
 `;
 
-export const Teste = styled.Text`
+export const Title = styled.Text`
    font-family: 'CircularStd-Black';
    font-size: 30px;
+   margin-top: ${StatusBar.currentHeight}px;
+   padding: 40px 0;
+`;
+
+export const PokeList = styled.View`
+   flex-direction: row;
+   flex-wrap: wrap;
+   justify-content: space-between;
 `;
