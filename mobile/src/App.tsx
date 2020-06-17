82ee67991725f8126cd57d8825a1aadab1192ca1
leandrosouzaa <leandror.dev@gmail.com>
2020-06-17 11:16:08 -0300

    config statusBar

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import {StatusBar} from 'react-native';
 import {Home} from './screens';
 
-const App: React.FC = () => <Home />;
+const App: React.FC = () => (
+   <>
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+      <Home />
+   </>
+);
 
 export default App;
