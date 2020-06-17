f4a5a4b740e1771cbec1f540b6c1950769a77436
leandrosouzaa <leandror.dev@gmail.com>
2020-06-17 16:41:09 -0300

    load pokemons from PokeApi & infinite scroll

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {Image} from 'react-native';
+import {Image, FlatList} from 'react-native';
+
+import api from '../../services/api';
 import {Pokemon} from '../../components';
+
 import {Container, Title, PokeList} from './styles';
 
+interface PokemonProps {
+   id: string;
+   url: string;
+   name: string;
+}
+
 const Home: React.FC = () => {
+   const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
+   const [page, setPage] = useState(0);
+
+   useEffect(() => {
+      async function loadPokemons(): Promise<void> {
+         const {data} = await api.get(`pokemon?offset=${page * 40}&limit=40`);
+
+         console.log(data);
+
+         const formattedPokemons = data.results.map((p: PokemonProps) => {
+            return {
+               ...p,
+               id: p.url.split('/')[p.url.split('/').length - 2],
+            };
+         });
+
+         setPokemons([...pokemons, ...formattedPokemons]);
+      }
+
+      loadPokemons();
+   }, [page]);
+
    return (
       <Container
          imageStyle={{
@@ -14,11 +45,22 @@ const Home: React.FC = () => {
             left: undefined,
          }}
          source={require('../../assets/images/pokeball.jpg')}>
-         <Title>Pokedex</Title>
-         <PokeList>
-            <Pokemon />
-            <Pokemon />
-         </PokeList>
+         <FlatList
+            ListHeaderComponent={<Title>Pokedex</Title>}
+            style={{flex: 1, borderRadius: 10}}
+            columnWrapperStyle={{
+               justifyContent: 'space-between',
+               marginBottom: 10,
+            }}
+            data={pokemons}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => <Pokemon data={item} />}
+            numColumns={2}
+            onEndReached={() => {
+               setPage((state) => state + 1);
+            }}
+            onEndReachedThreshold={0.3}
+         />
       </Container>
    );
 };
