4a52e880ba125e4ae60ea59b526c770d3989f89c
leandrosouzaa <leandror.dev@gmail.com>
2020-06-20 21:39:01 -0300

    detail screen

--- mobile/src/screens/Detail/index.tsx
+++ mobile/src/screens/Detail/index.tsx
@@ -1,32 +1,72 @@
 import React from 'react';
 
-import {View, Image} from 'react-native';
+import {useRoute, useNavigation} from '@react-navigation/native';
+import {Image, View, TouchableOpacity} from 'react-native';
 import {SharedElement} from 'react-navigation-shared-element';
-import {useRoute} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+   Container,
+   PokemonInfo,
+   Name,
+   TypeContainer,
+   TypeText,
+   Id,
+   PokemonGrid,
+   PokemonPicture,
+} from './styles';
 
 interface RouteParamsProps {
    data: {
       id: number;
       name: string;
    };
+   color: string;
 }
 
 const Detail: React.FC = () => {
    const {params} = useRoute();
+   const {goBack} = useNavigation();
 
-   const routeParams = params as RouteParamsProps;
+   const {data, color} = params as RouteParamsProps;
 
    return (
-      <View>
-         <SharedElement id={`pokemon-photo-${routeParams.data.id}`}>
-            <Image
-               style={{height: 150, width: 150}}
-               source={{
-                  uri: `https://pokeres.bastionbot.org/images/pokemon/${routeParams.data.id}.png`,
-               }}
-            />
-         </SharedElement>
-      </View>
+      <Container>
+         <PokemonInfo color={color}>
+            <TouchableOpacity activeOpacity={0.8} onPress={() => goBack()}>
+               <Icon size={24} color="#fff" name="arrow-left" />
+            </TouchableOpacity>
+            <PokemonGrid>
+               <View>
+                  <SharedElement id={`pokemon-title-${data.id}`}>
+                     <Name>
+                        {data.name.replace(/^./, data.name[0].toUpperCase())}
+                     </Name>
+                  </SharedElement>
+
+                  <TypeContainer>
+                     <TypeText>Grass</TypeText>
+                  </TypeContainer>
+               </View>
+               <SharedElement id={`pokemon-id-${data.id}`}>
+                  <Id>#{data.id.toString().padStart(3, '0')}</Id>
+               </SharedElement>
+            </PokemonGrid>
+            <PokemonPicture>
+               <SharedElement id={`pokemon-photo-${data.id}`}>
+                  <Image
+                     style={{
+                        height: 224,
+                        width: 224,
+                     }}
+                     source={{
+                        uri: `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`,
+                     }}
+                  />
+               </SharedElement>
+            </PokemonPicture>
+         </PokemonInfo>
+      </Container>
    );
 };
 
---
+++ mobile/src/screens/Detail/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+interface PokemonInfoProps {
+   color: string;
+}
+
+export const Container = styled.View`
+   flex: 1;
+`;
+
+export const PokemonInfo = styled.View<PokemonInfoProps>`
+   padding: 80px 24px 0 24px;
+
+   background: ${(props) => props.color};
+`;
+
+export const PokemonGrid = styled.View`
+   flex-direction: row;
+   justify-content: space-between;
+   padding-top: 32px;
+`;
+
+export const Name = styled.Text`
+   font-family: 'CircularStd-Black';
+   color: #fff;
+   font-size: 36px;
+`;
+
+export const TypeContainer = styled.View`
+   height: 24px;
+   width: 72px;
+   background: rgba(255, 255, 255, 0.2);
+   border-radius: 38px;
+   align-items: center;
+   justify-content: center;
+   margin-top: 8px;
+`;
+
+export const TypeText = styled.Text`
+   font-size: 12px;
+   font-family: 'CircularStd-Book';
+   color: #fff;
+`;
+
+export const Id = styled.Text`
+   font-family: 'CircularStd-Bold';
+   color: #fff;
+   font-size: 18px;
+   line-height: 50px;
+`;
+
+export const PokemonPicture = styled.View`
+   align-items: center;
+   margin-bottom: -32px;
+`;
